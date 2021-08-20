be33e932575b7d6c01008a109cc0fb04c4387200
fauky <fmented@gmail.com>
2021-08-21 04:55:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## Introduction
+midi-socket-server is a flask app that can be used as a midi server using [web socket](https://pypi.org/project/Flask-SocketIO/) and [rtmidi](https://pypi.org/project/python-rtmidi/)
+
+<hr>
+
+## Installation
+>required python
+
+```bash
+python -m venv env
+
+env/scripts/activate
+
+pip istall -r requirements.txt
+```
+
+<hr>
+
+## Preparation
+>Windows required [loopMIDI](https://www.tobias-erichsen.de/software/loopmidi.html) to be installed
+
+### For Windows Only
+open loopMIDI and create a new port and in ***.settings*** change **MIDI_NAME** variable with the same name as the port name 
+
+```bash
+#.settings
+
+MIDI_NAME="Your loopMIDI Port"
+```
+
+### For All System
+change **CLIENT_BASE_DIR** in ***.settings*** to public directory of [midi-socket-layout](https://github.com/fmented/midi-socket-layout) 
+
+```bash
+#.settings
+
+CLIENT_BASE_DIR="../path/to/layout/directory"
+```
+
+you can also change server port if you need to
+```bash
+#.settings
+
+SERVER_PORT=8080
+```
+
+lastly, create ***.env*** file in root directory
+and add **SECRET_KEY** and **FLASK_ENV** variables
+```bash
+#.env
+
+FLASK_ENV="development"
+#development or production
+
+SECRET_KEY="add your secret key here"
+```
+
+<hr>
+
+## Start
+to start the server type
+```bash
+python main.py
+```
+it will show a qrcode that can be scanned by other device that is in the same network to act as a MIDI contoller
---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import socket
+import sys
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
+from rtmidi import midiconstants as midi
+import rtmidi
+from dotenv import dotenv_values, load_dotenv
+import webbrowser
+import qrcode
+
+load_dotenv('.env')
+settings = dotenv_values(".settings")
+app = Flask(__name__, static_url_path='/{}'.format(settings['CLIENT_BASE_DIR']))
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+device = rtmidi.MidiOut()
+midi_ports = device.get_ports()
+
+
+try:
+    device.open_virtual_port(settings['MIDI_NAME'])
+except NotImplementedError:
+    name = list(filter(lambda n: n.startswith(settings['MIDI_NAME']), midi_ports))[0]
+    num = midi_ports.index(name)
+    device.open_port(num)
+    
+
+io = SocketIO(app)
+
+ip = socket.gethostbyname(socket.gethostname())
+port = settings['SERVER_PORT']
+url = 'http://{}:{}/'.format(ip, port)
+qrCode = qrcode.make(url)
+
+try:
+    qrCode.save("url.png")
+except OSError:
+    os.remove("url.png")
+    qrCode.save("url.png")
+
+@io.on('note_on')
+def send_note_on(data):
+    global device
+    device.send_message([midi.NOTE_ON+data['channel'], data['note'], data['velocity']])
+
+@io.on('note_off')
+def send_note_off(data):
+    global device
+    device.send_message([midi.NOTE_OFF+data['channel'], data['note'], data['velocity']])
+
+@io.on('all_note_off')
+def send_all_note_off(data):
+    global device
+    device.send_message([midi.CONTROLLER_CHANGE+data['channel'], midi.ALL_NOTES_OFF, 0])
+
+@io.on('cc')
+def send_cc(data):
+    global device
+    device.send_message([midi.CONTROLLER_CHANGE+data['channel'], data['control'], data['value']])
+
+@app.route('/')
+def base():
+    return send_from_directory(settings['CLIENT_BASE_DIR'], 'index.html')
+
+@app.route('/<path:path>')
+def home(path):
+    return send_from_directory(settings['CLIENT_BASE_DIR'], path)
+
+if __name__ == "__main__":
+
+    webbrowser.open("url.png")
+    io.run(app, host=ip, port=port)
